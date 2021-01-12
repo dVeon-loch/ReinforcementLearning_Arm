254f277a767c43ae7e27d22e2138d7adcdfd38f5
dVeon-loch <blochdevon@gmail.com>
2021-01-12 17:20:30 +0200

    Clean up and code simplification - Made it easier to run graphing script with static goal data - Updated readme

--- src/arm_bringup/scripts/results_graphing.py
+++ src/arm_bringup/scripts/results_graphing.py
@@ -9,8 +9,13 @@ import numpy as np
 from scipy.signal import savgol_filter
 os.chdir('../../..')
 
-SAClogfile = open('results/SACslow/20201111T005349.830047_SAC_/20201111T005349.907611.log','r')
+# Opening SAC logfile that contains all SAC data (was manually copied and pasted in from the various runs)
+
+# SAClogfile = open('results/SACslow/20201111T005349.830047_SAC_/20201111T005349.907611.log','r')
+SAClogfile = open('results/SAC_staticslow/20201110T180447.910284_SAC_/20201110T180447.996016.log','r')
 pattern = '.*Total Epi\:%{SPACE}%{BASE10NUM:episode_num} .*%{BASE10NUM:total_steps}.*Return\:%{SPACE}%{BASE10NUM:episode_return}.*'
+
+# Using grok to extract the needed datafields
 
 grok = Grok(pattern)
 
@@ -29,92 +34,83 @@ for i in matchlist:
         reward = i['episode_return']
         SACreturnarr.append(float(reward))
 del matchlist
-# TD3logfile = open('results/TD3_staticslow/20201110T191348.801774_TD3_/20201110T191348.868144.log','r')
-# pattern = '.*Total Epi\:%{SPACE}%{BASE10NUM:episode_num} .*%{BASE10NUM:total_steps}.*Return\:%{SPACE}%{BASE10NUM:episode_return}.*'
+TD3logfile = open('results/TD3_staticslow/20201110T191348.801774_TD3_/20201110T191348.868144.log','r')
+pattern = '.*Total Epi\:%{SPACE}%{BASE10NUM:episode_num} .*%{BASE10NUM:total_steps}.*Return\:%{SPACE}%{BASE10NUM:episode_return}.*'
 
-# grok = Grok(pattern)
+grok = Grok(pattern)
 
-# loglist = TD3logfile.readlines()
-# TD3logfile.close()
-# matchlist = []
-# for i in loglist:
-#     matchlist.append(grok.match(i))
-# del loglist
-# # print(matchlist)
-# TD3returnarr = []
-# for i in matchlist:
-#     if(i==None):
-#         pass
-#     else:
-#         reward = i['episode_return']
-#         TD3returnarr.append(float(reward))
-# del matchlist
-# PPOlogfile = open('results/PPO_static_slow/20201110T205822.677756_PPO_/20201110T205822.743626.log','r')
-# pattern = '.*Total Epi\:%{SPACE}%{BASE10NUM:episode_num} .*%{BASE10NUM:total_steps}.*Return\:%{SPACE}%{BASE10NUM:episode_return}.*'
+loglist = TD3logfile.readlines()
+TD3logfile.close()
+matchlist = []
+for i in loglist:
+    matchlist.append(grok.match(i))
+del loglist
+# print(matchlist)
+TD3returnarr = []
+for i in matchlist:
+    if(i==None):
+        pass
+    else:
+        reward = i['episode_return']
+        TD3returnarr.append(float(reward))
+del matchlist
+PPOlogfile = open('results/PPO_static_slow/20201110T205822.677756_PPO_/20201110T205822.743626.log','r')
+pattern = '.*Total Epi\:%{SPACE}%{BASE10NUM:episode_num} .*%{BASE10NUM:total_steps}.*Return\:%{SPACE}%{BASE10NUM:episode_return}.*'
 
-# grok = Grok(pattern)
+grok = Grok(pattern)
 
-# loglist = PPOlogfile.readlines()
-# PPOlogfile.close()
-# matchlist = []
-# for i in loglist:
-#     matchlist.append(grok.match(i))
-# del loglist
-# # print(matchlist)
-# PPOreturnarr = []
-# for i in matchlist:
-#     if(i==None):
-#         pass
-#     else:
-#         reward = i['episode_return']
-#         PPOreturnarr.append(float(reward))
-# del matchlist
+loglist = PPOlogfile.readlines()
+PPOlogfile.close()
+matchlist = []
+for i in loglist:
+    matchlist.append(grok.match(i))
+del loglist
+# print(matchlist)
+PPOreturnarr = []
+for i in matchlist:
+    if(i==None):
+        pass
+    else:
+        reward = i['episode_return']
+        PPOreturnarr.append(float(reward))
+del matchlist
 
-
-
-
+x = np.arange(max(len(SACreturnarr),len(TD3returnarr),len(PPOreturnarr)))
 
 
-
-
-# x = np.arange(max(len(SACreturnarr),len(TD3returnarr),len(PPOreturnarr)))
-x = np.arange(len(SACreturnarr))
-
+# Filtering raw data with Savitzky-Golay filter because training data is typically very noisy
 
 smoothedSAC = savgol_filter(SACreturnarr,101, 2)
-# smoothedTD3 = savgol_filter(TD3returnarr,101, 2)
-# smoothedPPO = savgol_filter(PPOreturnarr,101, 2)
+smoothedTD3 = savgol_filter(TD3returnarr,101, 2)
+smoothedPPO = savgol_filter(PPOreturnarr,101, 2)
 
 SACxlim = len(smoothedSAC)
-# TD3xlim = len(smoothedTD3)
-# PPOxlim = len(smoothedPPO)
+TD3xlim = len(smoothedTD3)
+PPOxlim = len(smoothedPPO)
 
+# PPO was trained for much longer which is why this was done, change this if necessary
 
-# difPPO_SAC = len(PPOreturnarr)-len(SACreturnarr)
-# for i in range(difPPO_SAC):
-#     SACreturnarr.append(None)
-#     smoothedSAC = np.append(smoothedSAC,None)
+difPPO_SAC = len(PPOreturnarr)-len(SACreturnarr)
+for i in range(difPPO_SAC):
+    SACreturnarr.append(None)
+    smoothedSAC = np.append(smoothedSAC,None)
 
-# difPPO_TD3 = len(PPOreturnarr)-len(TD3returnarr)
-# for i in range(difPPO_TD3):
-#     TD3returnarr.append(None)
-#     smoothedTD3 = np.append(smoothedTD3,None)
-
-
-
-
+difPPO_TD3 = len(PPOreturnarr)-len(TD3returnarr)
+for i in range(difPPO_TD3):
+    TD3returnarr.append(None)
+    smoothedTD3 = np.append(smoothedTD3,None)
 
 plt.figure()
-plt.title('Moving Goal Training Return')
+plt.title('Static Goal Training Return')
 
 plt.plot(x,SACreturnarr,'r',alpha=0.5)
 plt.plot(x,smoothedSAC, 'r' ,label="SAC")
-plt.xlim([0,SACxlim])
-# plt.plot(x,TD3returnarr,'g',alpha=0.5)
-# plt.plot(x,smoothedTD3,'g',label='TD3')
+# plt.xlim([0,SACxlim])
+plt.plot(x,TD3returnarr,'g',alpha=0.5)
+plt.plot(x,smoothedTD3,'g',label='TD3')
 # plt.xlim([0,TD3xlim])
-# plt.plot(x,PPOreturnarr,'b',alpha=0.5)
-# plt.plot(x,smoothedPPO,'b',label='PPO')
+plt.plot(x,PPOreturnarr,'b',alpha=0.5)
+plt.plot(x,smoothedPPO,'b',label='PPO')
 # plt.xlim([0,PPOxlim])
 plt.legend()
 plt.xlabel('Episodes') 
